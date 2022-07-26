04aff146ebc562767cff594130fb9d935694a166
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:38 +0200

    fix selector

--- lib/css/config/figcaption.php
+++ lib/css/config/figcaption.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-video figcaption' : '.wp-block-video figcaption',
+		'.wp-block-video figcaption',
 		array_merge(
 			$module->get_setting('figcaption_font')->get_css_data('font-family'),
 			$module->get_setting('figcaption_font_size')->get_css_data('font-size','','px'),
--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-video' : '.wp-block-video',
+		'.wp-block-video',
 		array_merge(
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
